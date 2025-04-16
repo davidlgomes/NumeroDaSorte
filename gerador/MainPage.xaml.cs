c178ed576e495d73c033ae7befee06c8b73cac95
DavidLopesGomes <DavidLopesGomes@ONRJL0086>
2025-04-16 11:06:01 -0300

    Adicionar arquivos de projeto.

---
+++ gerador/MainPage.xaml.cs
@@ -0,0 +1,35 @@
+namespace gerador;
+
+public partial class MainPage : ContentPage
+{
+    public MainPage()
+    {
+        InitializeComponent();
+    }
+
+    private void OnGeneratedLuckNumbers(object sender, EventArgs e)
+    {
+        NameApp.IsVisible = false;
+        ContainerLuckNumbers.IsVisible = true;
+        var set = GenerateLuckNumbers();
+
+        LuckNumber01.Text = set.ElementAt(0).ToString("D2");
+        LuckNumber02.Text = set.ElementAt(1).ToString("D2");
+        LuckNumber03.Text = set.ElementAt(2).ToString("D2");
+        LuckNumber04.Text = set.ElementAt(3).ToString("D2");
+        LuckNumber05.Text = set.ElementAt(4).ToString("D2");
+        LuckNumber06.Text = set.ElementAt(5).ToString("D2");
+    }
+
+    private SortedSet<int> GenerateLuckNumbers()
+    {
+        var list = new SortedSet<int>();
+        var random = new Random();
+        while (list.Count < 6)
+        {
+            var luckNumber = random.Next(1, 60);
+            list.Add(luckNumber);
+        }
+        return list;
+    }
+}
